9a76fb71441be907bed32536f42161a7450c0a00
lanasinergy <arkhabmaulana1@gmail.com>
2019-09-23 14:37:13 +0700

    Update drawable

--- app/src/main/java/com/example/project/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/project/diceroller/MainActivity.kt
@@ -22,11 +22,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
 
-//        val resultText: TextView = findViewById(R.id.result_text)
-
-        val randomInt = Random().nextInt(6) + 1
-
-        val drawableResource = when (randomInt) {
+        val drawableResource = when (Random().nextInt(6) + 1) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -34,7 +30,6 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-//        resultText.text = randomInt.toString()
         diceImage.setImageResource(drawableResource)
 
     }
